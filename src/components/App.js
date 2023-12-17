f24a021897d368c8c955fd3727340b28346e8710
Rohit-del-source <rohitverma18158920@gmail.com>
2023-12-17 10:18:55 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
+
 const App = () => {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [sum, setSum] = useState(0);
+
+  const handleChange1 = (event) => {
+    const parsedNumber1 = parseFloat(event.target.value) || 0;
+    setNumber1(parsedNumber1);
+    setSum(number1 + number2);
+  };
+
+  const handleChange2 = (event) => {
+    const parsedNumber2 = parseFloat(event.target.value) || 0;
+    setNumber2(parsedNumber2);
+    setSum(number1 + number2);
+  };
 
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
-       +
-      <input id='input2' onChange={}/> 
-      
-      <p id='result'></p>
+      <input id="input1" type="number" value={number1} onChange={handleChange1} />
+      +
+      <input id="input2" type="number" value={number2} onChange={handleChange2} />
+      =
+      <p id="result">{sum}</p>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
