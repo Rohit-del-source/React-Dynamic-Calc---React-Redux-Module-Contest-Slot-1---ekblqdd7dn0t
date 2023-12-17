4e59fe3edda3f2dbee78a454743d92ebe3864513
Rohit-del-source <rohitverma18158920@gmail.com>
2023-12-17 11:33:01 +0530

    second

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,24 @@
-import React, { useState } from 'react';
-import '../styles/App.css';
 
+import React,{useState,useEffect} from 'react'
+import '../styles/App.css';
 const App = () => {
-  const [number1, setNumber1] = useState(0);
-  const [number2, setNumber2] = useState(0);
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
   const [sum, setSum] = useState(0);
 
-  const handleChange1 = (event) => {
-    const parsedNumber1 = parseFloat(event.target.value) || 0;
-    setNumber1(parsedNumber1);
-    setSum(number1 + number2);
-  };
-
-  const handleChange2 = (event) => {
-    const parsedNumber2 = parseFloat(event.target.value) || 0;
-    setNumber2(parsedNumber2);
-    setSum(number1 + number2);
-  };
-
+  useEffect(()=>{
+    setSum(Number(input1)+Number(input2));
+  },[input1, input2])
   return (
     <div id="main">
-      <input id="input1" type="number" value={number1} onChange={handleChange1} />
-      +
-      <input id="input2" type="number" value={number2} onChange={handleChange2} />
-      =
-      <p id="result">{sum}</p>
+      <input id='input1' value={input1} onChange={e=>setInput1(e.target.value)}/>
+       +
+      <input id='input2' value={input2} onChange={e=>setInput2(e.target.value)}/> 
+      
+      <p id='result'>{sum}</p>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
